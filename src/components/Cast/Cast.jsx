2067e8afe0e32a84601798ee32640fe1e420b109
Andrew Butenko <sedkol88@gmail.com>
2024-02-06 15:42:10 +0200

    update cast
    додано перевірку наявності фото актора

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -28,12 +28,19 @@ const Cast = () => {
     fetchPost();
   }, [id]);
 
+  const defaultImg =
+    '<https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700>';
+
   const elements = casts.map(({ id, name, character, profile_path }) => (
     <li key={id} className={styles.item}>
       <p>{name}</p>
       <p>{character}</p>
       <img
-        src={`https://image.tmdb.org/t/p/w500${profile_path}`}
+        src={
+          profile_path
+            ? `https://image.tmdb.org/t/p/w500${profile_path}`
+            : defaultImg
+        }
         alt="actor"
         width="100"
       />
